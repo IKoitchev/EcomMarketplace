c8c961bdfce7471f8b3a9af6faa9bc4c78ec01ba
Ivan Koitchev <ivan_koitchev@abv.bg>
2022-09-26 14:17:04 +0200

    Added docker files Updated package.json

--- Product microservice/config/default.ts
+++ Product microservice/config/default.ts
@@ -1,5 +1,7 @@
 export default {
   port: 3008,
   host: 'localhost',
-  dbUri: 'mongodb://localhost:27017/Ecom-products',
+  //dbUri: 'mongodb://localhost:27017/Ecom-products',
+  dbUri:
+    'mongodb+srv://admin:admin@cluster0.ldsu16h.mongodb.net/Ecom?retryWrites=true&w=majority',
 };
