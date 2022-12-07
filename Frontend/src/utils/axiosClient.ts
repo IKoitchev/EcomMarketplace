28d064e80f2048a4d0f9cb87870189e9da3bb62c
Ivan Koitchev <ivan_koitchev@abv.bg>
2022-12-07 12:13:55 +0100

    updated manifest files

--- Frontend/src/utils/axiosClient.ts
+++ Frontend/src/utils/axiosClient.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
-const gatewayURL = 'http://localhost:8080';
+// const gatewayURL = 'http://localhost:8080';
+const gatewayURL = 'http://35.227.219.118/';
 
 const axiosClient: AxiosInstance = axios.create({
   baseURL: gatewayURL,
